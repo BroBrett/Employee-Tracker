9098619df638e87d95d4468eab776a5cba34ba00
brett hockridge <bretthockridge98@gmail.com>
2022-05-19 20:36:12 -0700

    added more to index.js

--- index.js
+++ index.js
@@ -2,3 +2,38 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const mysql = require("mysql2");
 const db = require("./db");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "root",
+    database: "employee_trackerDB",
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected");
+    init();
+});
+
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "options",
+            message: "Choose an option:",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add department",
+                "Add role",
+                "Add employee",
+                "Update employee role",
+                "Bye!",
+            ]
+        }
+    ])
+}
