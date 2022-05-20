53031562f05c1c44ec5635198ad01bf40e67c2eb
brett hockridge <bretthockridge98@gmail.com>
2022-05-20 13:17:10 -0700

    a lot of changes and fixes to whole db file and index.js

--- index.js
+++ index.js
@@ -1,142 +1,132 @@
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
-const mysql = require("mysql2");
 const db = require("./db");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3001,
-    user: "root",
-    password: "root",
-    database: "employee_trackerDB",
-});
-
 async function mainMenu() {
-    const { choices } = await inquirer.prompt([
-      {
-        type: "list",
-        name: "choices",
-        message: "What would you like to do?",
-        choices: [
-          "View all Departments",
-          "View all Roles",
-          "View all Employees",
-          "Add Department",
-          "Add Role",
-          "Add Employee",
-          "Update Employee Role",
-          "Exit",
-        ],
-      },
-    ]);
-    switch (choices) {
-      case "View all Departments":
-        return viewAllDepartments();
-      case "View all Roles":
-        return viewAllRoles();
-      case "View all Employees":
-        return viewAllEmployees();
-      case "Add Department":
-        return addDepartment();
-      case "Add Role":
-        return addRole();
-      case "Add Employee":
-        return addEmployee();
-      case "Update Employee Role":
-        return updateEmployeeRole();
-      case "Exit":
-        return mainMenu();
-    }
+  const { choices } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "choices",
+      message: "What would you like to do?",
+      choices: [
+        "View all Departments",
+        "View all Roles",
+        "View all Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Role",
+        "Exit",
+      ],
+    },
+  ]);
+  switch (choices) {
+    case "View all Departments":
+      return viewAllDepartments();
+    case "View all Roles":
+      return viewAllRoles();
+    case "View all Employees":
+      return viewAllEmployees();
+    case "Add Department":
+      return addDepartment();
+    case "Add Role":
+      return addRole();
+    case "Add Employee":
+      return addEmployee();
+    case "Update Employee Role":
+      return updateEmployeeRole();
+    case "Exit":
+      return mainMenu();
   }
+}
 
-    async function viewAllDepartments() {
-        const [departments] = await db.viewAllDepartments();
-        console.table(departments);
-        mainMenu();
-      }
-      
-      async function viewAllRoles() {
-        const [roles] = await db.viewAllRoles();
-        console.table(roles);
-        mainMenu();
-      }
-      
-      async function viewAllEmployees() {
-        const [employees] = await db.viewAllEmployees();
-        console.table(employees);
-        mainMenu();
-      }
-      
-      async function addDepartment() {
-        const department = await inquirer.prompt([
-          {
-            type: "input",
-            name: "name",
-            message: "What is the name of the Department?",
-          },
-        ]);
-        await db.addDepartment(department);
-        console.log("New department added");
-        mainMenu();
-      }
-      
-      async function addRole() {
-        const departments = await db.viewAllDepartments();
-        console.log(departments);
-        const departmentChoices = departments.map(({ id, name }) => ({
-          department: name,
-          id: id,
-        }));
-        console.log(departmentChoices);
-        const role = await inquirer.prompt([
-          {
-            type: "input",
-            name: "title",
-            message: "What is the title of the role?",
-          },
-          {
-            type: "input",
-            name: "salary",
-            message: "what is the salary of the title?",
-          },
-          {
-            type: "list",
-            name: "department_id",
-            message: "Which department does the role belong to?",
-            choices: departmentChoices,
-          },
-        ]);
-        await db.addRole(role);
-        console.log(`Added ${title.name} to the database`);
-        mainMenu();
-      }
-      
-      async function addEmployee() {
-        const employee = await inquirer.prompt([
-          {
-            type: "input",
-            name: "first_name",
-            message: "What is the employee's first name?",
-          },
-          {
-            type: "input",
-            name: "last_name",
-            message: "What is the employee's last name?",
-          },
-          {
-            type: "list",
-            name: "role_id",
-            message: "What is the employee's role??",
-            choices: selectRole(),
-          },
-          {
-            type: "list",
-            name: "manager_id",
-            message: "Who is the employee's manager?",
-            choices: selectManager(),
-          },
-        ]);
-        mainMenu();
+async function viewAllDepartments() {
+  const [departments] = await db.viewAllDepartments();
+  console.table(departments);
+  mainMenu();
+}
+
+async function viewAllRoles() {
+  const [roles] = await db.viewAllRoles();
+  console.table(roles);
+  mainMenu();
+}
+
+async function viewAllEmployees() {
+  const [employees] = await db.viewAllEmployees();
+  console.table(employees);
+  mainMenu();
+}
+
+async function addDepartment() {
+  const department = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of the Department?",
+    },
+  ]);
+  await db.addDepartment(department);
+  console.log("New department added");
+  mainMenu();
+}
+
+async function addRole() {
+  const departments = await db.viewAllDepartments();
+  console.log(departments);
+  const departmentChoices = departments.map(({ id, name }) => ({
+    department: name,
+    id: id,
+  }));
+  console.log(departmentChoices);
+  const role = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of the role?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "what is the salary of the title?",
+    },
+    {
+      type: "list",
+      name: "department_id",
+      message: "Which department does the role belong to?",
+      choices: departmentChoices,
+    },
+  ]);
+  await db.addRole(role);
+  console.log(`Added ${title.name} to the database`);
+  mainMenu();
+}
+
+async function addEmployee() {
+  const employee = await inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "What is the employee's first name?",
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "What is the employee's last name?",
+    },
+    {
+      type: "list",
+      name: "role_id",
+      message: "What is the employee's role??",
+      choices: selectRole(),
+    },
+    {
+      type: "list",
+      name: "manager_id",
+      message: "Who is the employee's manager?",
+      choices: selectManager(),
+    },
+  ]);
+  mainMenu();
 }
 
 mainMenu();
